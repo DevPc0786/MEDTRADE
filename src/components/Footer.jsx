59fa5abf08ca25967f051d65587ae35b0451fa8e
Rahul kumar maury <rahu7827759@gmail.com>
2024-12-16 22:38:48 +0530

    design footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="grid grid-cols-4 mt-4 border-t-2 pt-14 ">
+        <div>
+          <h2 className="font-bold text-sm">Contact Us</h2>
+          <p>Call us 24/7 </p>
+          <p>011-2552 xxxx</p>
+          <p>sec - 12, Noida/Delhi NCR</p>
+        </div>
+        <div>
+          <h2 className="font-bold text-sm">Medirent Bussiness</h2>
+        </div>
+        <div>
+          <h2 className="font-bold text-sm">Company</h2>
+        </div>
+        <div>
+          <h2 className="font-bold text-sm">Join Medirent Wellness Club</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
