f36a56100269b15b63c00245a7a3a4aa1c189585
Rahul kumar maury <rahu7827759@gmail.com>
2024-12-19 01:42:16 +0530

    added slider

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -4,22 +4,45 @@ import { FaRegThumbsUp } from "react-icons/fa";
 import { TbTruck } from "react-icons/tb";
 import { MdPayment } from "react-icons/md";
 import { TbMessageCircle } from "react-icons/tb";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HomeSlider = () => {
+  const settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+  const images = [
+    "https://medirent.co.in/wp-content/uploads/2022/02/banner1-1.jpg",
+    "https://medirent.co.in/wp-content/uploads/2022/02/banner1.jpg",
+  ];
+
   return (
     <div>
       <div className="flex mt-5 bg-white">
-        <div className="w-[70%] h-[450px]">
-          <div className="h-[85%]">
-            <img
-              src="https://medirent.co.in/wp-content/uploads/2022/02/banner1-1.jpg"
-              alt="Banner Image"
-            />
-            {/* <img
-            className="w-full"
-            src="https://medirent.co.in/wp-content/uploads/2022/02/banner1.jpg"
-            alt="Banner Image"
-          /> */}
+        <div className="w-[70%] h-[550px]">
+          <div className="">
+            <div className="slider-container">
+              <Slider {...settings}>
+                {images.map((image, index) => (
+                  <div key={index}>
+                    <img
+                      src={image}
+                      alt={`Slide ${index + 1}`}
+                      style={{ width: "100%", height: "465px" }}
+                    />
+                  </div>
+                ))}
+              </Slider>
+            </div>
 
             <div className="pb-5 h-full">
               <div className="flex justify-between p-4">
@@ -64,7 +87,7 @@ const HomeSlider = () => {
           </div>
         </div>
 
-        <div className="w-[30%] h-[450px] gap-5">
+        <div className="w-[30%] h-[550px] gap-5">
           <img
             className="w-full h-[50%] pl-5 pb-[0.625rem]"
             src="https://medirent.co.in/wp-content/uploads/2022/02/banner_mask-1.jpg"
