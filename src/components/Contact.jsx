0036098e66c43a7ea231f2658943fd669d5041cf
Rahul kumar maury <rahu7827759@gmail.com>
2024-12-26 23:58:50 +0530

    contact page

---
+++ src/components/Contact.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <div className="flex justify-evenly items-center mt-12">
+        <div>
+          <form action="" className="text-sm font-light text-[#948c94]">
+            <p className="my-3">
+              <label htmlFor="">Your Name </label> <br />
+              <span>
+                <input
+                  type="name"
+                  className="py-3 border border-gray-300 w-96"
+                />
+              </span>
+            </p>
+
+            <p className="my-3">
+              <label htmlFor="">Your Email </label> <br />
+              <span>
+                <input
+                  type="name"
+                  className="py-3 border border-gray-300 w-96"
+                />
+              </span>
+            </p>
+            <p className="my-3">
+              <label htmlFor="">Mobile </label> <br />
+              <span>
+                <input
+                  type="name"
+                  className="py-3 border border-gray-300 w-96"
+                />
+              </span>
+            </p>
+            <p className="my-3">
+              <label htmlFor="">Buy </label> <br />
+              <span>
+                <input
+                  type="name"
+                  className="py-3 border border-gray-300 w-96"
+                />
+              </span>
+            </p>
+            <p className="my-3">
+              <label htmlFor="">Message (optional) </label> <br />
+              <span>
+                <textarea
+                  type="name"
+                  rows={5}
+                  className="py-3 border border-gray-300 w-96 "
+                ></textarea>
+              </span>
+            </p>
+
+            <button className=" px-6 py-3 text-white bg-[#3d83c0]">
+              Send Enquiry
+            </button>
+          </form>
+        </div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14014.74893667879!2d77.3260048!3d28.5355161!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ceff1f1d9e5b3%3A0x2b8f1e9e8b8e8e8e!2sNoida%2C%20Uttar%20Pradesh%2C%20India!5e0!3m2!1sen!2sus!4v1634567890123!5m2!1sen!2sus"
+          width="600"
+          height="450"  
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
